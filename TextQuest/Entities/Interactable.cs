e99e1f1d386caf2132535c76908cf93568ccbab9
Abbegubbegum <albin.nojback@gmail.com>
2022-04-04 09:28:44 +0200

    Add Class Diagram

--- TextQuest/Entities/Interactable.cs
+++ TextQuest/Entities/Interactable.cs
@@ -5,7 +5,7 @@ namespace TextQuest.Entities
 {
     public class Interactable : Gameobject
     {
-        public string Name { get; protected set; }
+        public string Name { get; init; }
 
         public Interactable(int x, int y, int width, int height, string name, Color c = default) : base(x, y, width, height, c)
         {
